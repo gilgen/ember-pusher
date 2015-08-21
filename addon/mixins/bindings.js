0ff16404a5b4b306cbe2c5d6bb09fa240ed2438f
Martin Muñoz <im.mmun@gmail.com>
2015-08-20 23:41:41 -0400

    Use Object.keys instead of Ember.keys

--- addon/mixins/bindings.js
+++ addon/mixins/bindings.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Mixin.create({
   init() {
     if (this.PUSHER_SUBSCRIPTIONS) {
-      Ember.keys(this.PUSHER_SUBSCRIPTIONS).forEach(channelName => {
+      Object.keys(this.PUSHER_SUBSCRIPTIONS).forEach(channelName => {
         let events = this.PUSHER_SUBSCRIPTIONS[channelName];
         this.pusher.wire(this, channelName, events);
       });
@@ -14,7 +14,7 @@ export default Ember.Mixin.create({
 
   willDestroy() {
     if (this.PUSHER_SUBSCRIPTIONS) {
-      Ember.keys(this.PUSHER_SUBSCRIPTIONS).forEach(channelName => {
+      Object.keys(this.PUSHER_SUBSCRIPTIONS).forEach(channelName => {
         this.pusher.unwire(this, channelName);
       });
     }
